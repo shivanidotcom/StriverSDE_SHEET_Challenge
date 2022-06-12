3ad7632e351e7539785d683f63e96c8368a46528
Shivani Vemula <76956616+shivanidotcom@users.noreply.github.com>
2022-06-12 15:19:28 +0530

    Create Cycle Detection in a Singly Linked List.java

---
+++ Cycle Detection in a Singly Linked List.java
@@ -0,0 +1,38 @@
+import java.util.* ;
+import java.io.*; 
+/*  
+
+    Following is the representation of the Singly Linked List node
+
+    class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+*/
+
+public class Solution {
+    
+    public static boolean detectCycle(Node<Integer> head) {
+        //Your code goes here
+        //cycle detection
+        Node<Integer> fast = head;
+        Node<Integer> slow = head;
+        if(head.next == null || head == null){
+            return false;
+        }
+        while(fast!=null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
